7f5625819974ea8b254713b4515c813e9f3380c5
LESPINASSE MARINI <lespinasse.laurie5@gmail.com>
2023-10-13 16:23:54 +0200

    amelioration du code JS

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-let motApplication = "Bonjour";
-let motUtilisateur = prompt("Entrez un mot :" + motApplication);
 
-if (motUtilisateur === motApplication) {
-    console.log("Bravo !")
-} else {
-    console.log("Vous avez fait une erreur de frappe !")
+let listeMots = ["Cachalot", "Pétunia", "Serviette"];
+let listePhrases = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"];
+let score = 0; 
+
+let choixListe = prompt("Voulez-vous une liste de mots ou de phrases ? Entrez mots ou phrases pour faire votre choix");
+
+while (choixListe !== "mots" && choixListe !== "phrases") {
+    choixListe = prompt("Voulez-vous une liste de mots ou de phrases ? Entrez mots ou phrases pour faire votre choix");
 }
+
+if (choixListe === "mots") {
+    for (let i = 0; i < listeMots.length; i++) {
+        motUtilisateur = prompt("Entrez un mot : " + listeMots[i]);
+        if (motUtilisateur === listeMots[i]) {
+            score++
+        }
+    }
+} else {
+    for (let i = 0; i < listePhrases.length; i++) {
+        motUtilisateur = prompt("Entrez une phrase : " + listePhrases[i]);
+        if (motUtilisateur === listePhrases[i]) {
+            score++
+        }
+    }    
+}
+
+console.log(score);
