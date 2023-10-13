4ec2fd4eefea3205da1c8a5444e8f0c6580e58d0
LESPINASSE MARINI <lespinasse.laurie5@gmail.com>
2023-10-13 14:59:57 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let motApplication = "Bonjour";
+let motUtilisateur = prompt("Entrez un mot :" + motApplication);
+
+if (motUtilisateur === motApplication) {
+    console.log("Bravo !")
+} else {
+    console.log("Vous avez fait une erreur de frappe !")
+}
