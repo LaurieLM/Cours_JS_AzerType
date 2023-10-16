d6fc567d2491ed125abcdecf19a9de3f11304e03
LESPINASSE MARINI <lespinasse.laurie5@gmail.com>
2023-10-16 17:17:24 +0200

    finish

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
 lancerJeu()
+
+// let inputEcriture = document.getElementById("inputEcriture");
+// let btnValiderMot = document.getElementById("btnValiderMot");
+
+// let zoneProposition = document.querySelector(".zoneProposition");
+// let spanScore = document.querySelector(".zoneScore span");
+
+// let listeBtnRadio = document.querySelectorAll(".optionSource input");
